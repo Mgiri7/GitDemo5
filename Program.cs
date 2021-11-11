b050419a0d85a9779fe95ff11c2a1590766b79b1
giri <girim7593@gmail.com>
2021-11-11 19:19:07 +0530

    New Changes

--- Program.cs
+++ Program.cs
@@ -8,10 +8,12 @@ namespace Demo
        static void Main()
         {
             int a = 10;
+            int z=20;
             int? b = null;
             int? c = 100;
 
             Console.WriteLine(a);
+            Console.WriteLine(z);
             Console.WriteLine(b);
 
             int k = b?? 0;

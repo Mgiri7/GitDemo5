cbb10fe5063d9f62c422d20ff14ba5f4bea7ec2b
giri <girim7593@gmail.com>
2021-11-11 19:12:48 +0530

    Initail Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Main method
+namespace Demo
+{
+    class Program
+    {
+       static void Main()
+        {
+            int a = 10;
+            int? b = null;
+            int? c = 100;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int k = b?? 0;
+            Console.WriteLine(k);
+            int y = c ?? 0;
+            Console.WriteLine(y);
+        }
+    }
+}
